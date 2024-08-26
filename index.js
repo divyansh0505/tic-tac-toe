1100c710bc0717df14e4ea6fccffcf07f70350df
divyansh0505 <divyanshsachdeva198@gmail.com>
2024-08-26 17:07:51 +0530

    code finished

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 let boxes=document.querySelectorAll(".box");
 let reset=document.querySelector(".resetbtn");
-let newbtn=document.querySelector("")
+let newbtn=document.querySelector("#newgame");
+let msgcontainer=document.querySelector(".msgcontainer");
+let msg=document.querySelector("#msg");
 const winPatterns = [
     [0, 1, 2],
     [3, 4, 5],
@@ -27,8 +29,37 @@ boxes.forEach((box)=>{
     })
 })
 
+const resetgame=()=>{
+    turnO=true;
+    enableboxes();
+    msgcontainer.classList.add("hide");
+    boxes.forEach(box => box.classList.remove("win-sequence"));
+}
+
+const disableboxes=()=>{
+    for(let box of boxes){
+        box.disabled=true;
+    }
+}
+
+const enableboxes=()=>{
+    for(let box of boxes){
+        box.disabled=false;
+        box.innerText="";
+    }
+}
+
+const showWinner=(winner)=>{
+    msg.innerText=`Congratulation winner is ${winner}`;
+    msgcontainer.classList.remove("hide");
+    disableboxes();
+}
+
 const checkwinner=()=>{
     for(let pattern of winPatterns){
+        let pos1 = boxes[pattern[0]];
+        let pos2 = boxes[pattern[1]];
+        let pos3 = boxes[pattern[2]];
         let pos1val=boxes[pattern[0]].innerText;
         let pos2val=boxes[pattern[1]].innerText;
         let pos3val=boxes[pattern[2]].innerText;
@@ -36,9 +67,19 @@ const checkwinner=()=>{
     if(pos1val!="" && pos2val!="" && pos3val!=""){
         if(pos1val==pos2val && pos2val==pos3val){
             console.log("Winner",pos1val);
+            pos1.classList.add("win-sequence");
+            pos2.classList.add("win-sequence");
+            pos3.classList.add("win-sequence");
+            showWinner(pos1val);
         }
     }
-}
-}
+  }
+};
+
+newbtn.addEventListener("click",resetgame);
+reset.addEventListener("click",resetgame);
+
+
+
 
 
